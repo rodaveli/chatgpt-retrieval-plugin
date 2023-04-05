828ba4441c2018468fe1bd00ac1eb4363d20c784
rodrigoabreu56 <rodrigo@itg-advisory.com>
2023-04-05 11:54:57 -0500

    c

--- server/main.py
+++ server/main.py
@@ -1,3 +1,7 @@
+# This is a version of the main.py file found in ../../server/main.py that also gives ChatGPT access to the upsert endpoint
+# (allowing it to save information from the chat back to the vector) database.
+# Copy and paste this into the main file at ../../server/main.py if you choose to give the model access to the upsert endpoint
+# and want to access the openapi.json when you run the app locally at http://0.0.0.0:8000/sub/openapi.json.
 import os
 from typing import Optional
 import uvicorn
@@ -18,6 +22,7 @@ from services.file import get_document_from_file
 
 from models.models import DocumentMetadata, Source
 
+
 bearer_scheme = HTTPBearer()
 BEARER_TOKEN = os.environ.get("BEARER_TOKEN")
 assert BEARER_TOKEN is not None
@@ -29,10 +34,10 @@ def validate_token(credentials: HTTPAuthorizationCredentials = Depends(bearer_sc
     return credentials
 
 
-app = FastAPI(dependencies=[Depends(validate_token)])
+app = FastAPI()
 app.mount("/.well-known", StaticFiles(directory=".well-known"), name="static")
 
-# Create a sub-application, in order to access just the query endpoint in an OpenAPI schema, found at http://0.0.0.0:8000/sub/openapi.json when the app is running locally
+# Create a sub-application, in order to access just the upsert and query endpoints in the OpenAPI schema, found at http://0.0.0.0:8000/sub/openapi.json when the app is running locally
 sub_app = FastAPI(
     title="Retrieval Plugin API",
     description="A retrieval API for querying and filtering documents based on natural language queries and metadata",
@@ -74,8 +79,27 @@ async def upsert_file(
     "/upsert",
     response_model=UpsertResponse,
 )
+async def upsert_main(
+    request: UpsertRequest = Body(...),
+    token: HTTPAuthorizationCredentials = Depends(validate_token),
+):
+    try:
+        ids = await datastore.upsert(request.documents)
+        return UpsertResponse(ids=ids)
+    except Exception as e:
+        print("Error:", e)
+        raise HTTPException(status_code=500, detail="Internal Service Error")
+
+
+@sub_app.post(
+    "/upsert",
+    response_model=UpsertResponse,
+    # NOTE: We are describing the shape of the API endpoint input due to a current limitation in parsing arrays of objects from OpenAPI schemas. This will not be necessary in the future.
+    description="Save chat information. Accepts an array of documents with text (potential questions + conversation text), metadata (source 'chat' and timestamp, no ID as this will be generated). Confirm with the user before saving, ask for more details/context.",
+)
 async def upsert(
     request: UpsertRequest = Body(...),
+    token: HTTPAuthorizationCredentials = Depends(validate_token),
 ):
     try:
         ids = await datastore.upsert(request.documents)
@@ -91,6 +115,7 @@ async def upsert(
 )
 async def query_main(
     request: QueryRequest = Body(...),
+    token: HTTPAuthorizationCredentials = Depends(validate_token),
 ):
     try:
         results = await datastore.query(
@@ -110,6 +135,7 @@ async def query_main(
 )
 async def query(
     request: QueryRequest = Body(...),
+    token: HTTPAuthorizationCredentials = Depends(validate_token),
 ):
     try:
         results = await datastore.query(
@@ -127,6 +153,7 @@ async def query(
 )
 async def delete(
     request: DeleteRequest = Body(...),
+    token: HTTPAuthorizationCredentials = Depends(validate_token),
 ):
     if not (request.ids or request.filter or request.delete_all):
         raise HTTPException(
